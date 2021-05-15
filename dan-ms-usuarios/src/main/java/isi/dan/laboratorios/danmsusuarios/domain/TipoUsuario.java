eea321df5d3e7b196e537992794ef1bb9111e5fe
Ale Guiter <aleguiter9@gmail.com>
2021-05-15 19:37:21 -0300

    Domain package created, classes: Cliente, Empleado, Obra, Usuario, TipoUsuario, TipoObra

---
+++ dan-ms-usuarios/src/main/java/isi/dan/laboratorios/danmsusuarios/domain/TipoUsuario.java
@@ -0,0 +1,20 @@
+package isi.dan.laboratorios.danmsusuarios.domain;
+
+public class TipoUsuario {
+    
+    private Integer id;
+    private String tipo;
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
---
+++ dan-ms-usuarios/src/main/java/isi/dan/laboratorios/danmsusuarios/domain/Usuario.java
@@ -0,0 +1,41 @@
+package isi.dan.laboratorios.danmsusuarios.domain;
+
+public class Usuario {
+    
+    private Integer id;
+    private String user, password;
+    private TipoUsuario tipoUsuario;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public TipoUsuario getTipoUsuario() {
+		return tipoUsuario;
+	}
+	public void setTipoUsuario(TipoUsuario tipoUsuario) {
+		this.tipoUsuario = tipoUsuario;
+	}
+
+
+}
