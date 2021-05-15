010303758976434f38eb78da52d1b093fb22aac7
Ale Guiter <aleguiter9@gmail.com>
2021-05-15 20:15:47 -0300

    added GET by cuit to endpoint: api/cliente/cuit and tested with postman

---
+++ dan-ms-usuarios/src/main/java/isi/dan/laboratorios/danmsusuarios/rest/ClienteRest.java
@@ -0,0 +1,89 @@
+package isi.dan.laboratorios.danmsusuarios.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import isi.dan.laboratorios.danmsusuarios.domain.Cliente;
+
+@RestController
+@RequestMapping(ClienteRest.API_CLIENTE)
+public class ClienteRest {
+    static final String API_CLIENTE = "/api/cliente";
+    
+    private static final List<Cliente> listaClientes = new ArrayList<>();
+    private static Integer ID_GEN = 1;
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Cliente> clientePorId(@PathVariable Integer id){
+
+        Optional<Cliente> c =  listaClientes
+                .stream()
+                .filter(unCli -> unCli.getId().equals(id))
+                .findFirst();
+        return ResponseEntity.of(c);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Cliente>> todos(){
+        return ResponseEntity.ok(listaClientes);
+    }
+
+    @GetMapping(path = "/cuit/{cuit}")
+    public ResponseEntity<Cliente> clientePorCuit(@PathVariable String cuit) {
+        Optional<Cliente> c =  listaClientes
+                .stream()
+                .filter(unCli -> unCli.getCuit().equals(cuit))
+                .findFirst();
+        return ResponseEntity.of(c);
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> crear(@RequestBody Cliente nuevo){
+    	System.out.println("Crear cliente "+ nuevo);
+        nuevo.setId(ID_GEN++);
+        listaClientes.add(nuevo);
+        return ResponseEntity.ok(nuevo);
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<Cliente> actualizar(@RequestBody Cliente nuevo,  @PathVariable Integer id) {
+        OptionalInt indexOpt =   IntStream.range(0, listaClientes.size())
+        .filter(i -> listaClientes.get(i).getId().equals(id))
+        .findFirst();
+
+        if(indexOpt.isPresent()){
+            listaClientes.set(indexOpt.getAsInt(), nuevo);
+            return ResponseEntity.ok(nuevo);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Cliente> borrar(@PathVariable Integer id){
+        OptionalInt indexOpt =   IntStream.range(0, listaClientes.size())
+        .filter(i -> listaClientes.get(i).getId().equals(id))
+        .findFirst();
+
+        if(indexOpt.isPresent()){
+            listaClientes.remove(indexOpt.getAsInt());
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+}
