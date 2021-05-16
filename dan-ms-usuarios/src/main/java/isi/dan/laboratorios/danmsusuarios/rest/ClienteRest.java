3400dc5c47c1d8d49728dd6f53af58a4366b2349
Ale Guiter <aleguiter9@gmail.com>
2021-05-16 19:41:23 -0300

    Added new request to the client api, GET by cuit and GET by razonSocial

--- dan-ms-usuarios/src/main/java/isi/dan/laboratorios/danmsusuarios/rest/ClienteRest.java
+++ dan-ms-usuarios/src/main/java/isi/dan/laboratorios/danmsusuarios/rest/ClienteRest.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.OptionalInt;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import isi.dan.laboratorios.danmsusuarios.domain.Cliente;
@@ -41,6 +44,7 @@ public class ClienteRest {
         return ResponseEntity.ok(listaClientes);
     }
 
+    // GET por cuit -> Retorna un único cliente
     @GetMapping(path = "/cuit/{cuit}")
     public ResponseEntity<Cliente> clientePorCuit(@PathVariable String cuit) {
         Optional<Cliente> c =  listaClientes
@@ -48,6 +52,19 @@ public class ClienteRest {
                 .filter(unCli -> unCli.getCuit().equals(cuit))
                 .findFirst();
         return ResponseEntity.of(c);
+    }
+
+    // GET por razon social (query string OPC) -> Retorna una lista de clientes con la razon social especificada, example: "api/cliente/razonsocial?razonSocial=example"
+    @GetMapping(path = "/razonsocial")
+    @ResponseBody
+    public ResponseEntity<List<Cliente>> clientePorRazonSocial(
+        @RequestParam(required = false) String razonSocial) {
+        List<Cliente> c =  listaClientes
+                .stream()
+                .filter(unCli -> unCli.getRazonSocial().equals(razonSocial))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(c);
     }
 
     @PostMapping
